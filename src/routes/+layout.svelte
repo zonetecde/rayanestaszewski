0418be8b7b753dc843d3b1ca1337044d4c397003
zonetecde <zonetecde@gmail.com>
2023-12-26 22:20:26 +0100

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import '../app.css';
+</script>
+
+<div class="bg-[#443C68] h-screen w-screen">
+	<slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<header class="w-screen h-24 bg-[#18122B]"></header>
+
+<section class="w-screen h-[60%] bg-[#252235] p-5 grid grid-cols-6 grid-rows-6">
+	<p class="col-start-2 text-7xl row-start-2 font-bold">Hey!</p>
+</section>
